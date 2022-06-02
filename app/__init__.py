d0b1984f57185609479faee2d8eb0a7cc7e5db79
xuo <61861557+xuo-lynn@users.noreply.github.com>
2022-06-02 15:25:30 -0400

    Main Page and Nav Bar
    sheeeesh

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Gorilla Gang Portfolio", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About Me", url=os.getenv("URL"))
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html', title="Experience", url=os.getenv("URL"))
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title="Projects", url=os.getenv("URL"))
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html', title="Skills", url=os.getenv("URL"))
+
