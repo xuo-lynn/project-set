120b610e1639349f59343585b1e3eb3b859ca082
xuo <61861557+xuo-lynn@users.noreply.github.com>
2022-06-12 23:41:28 -0400

    overhaul

--- app/__init__.py
+++ app/__init__.py
@@ -8,39 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Gorilla Gang Portfolio", url=os.getenv("URL"))
+    return render_template('index.html', title="Set's Portfolio", url=os.getenv("URL"))
 
-@app.route('/xuo')
-def xuo():
-    return render_template('xuo.html', title="Set Lynn Portfolio")
-
-@app.route('/xuo/hobbies')
-def xuo_jinja():
-    return render_template('xuo_jinja.html', title="Set Hobbies")
-
-@app.route('/chuu')
-def chuu():
-    return render_template('chuu.html', title="Kaitlyn Chau Portfolio")
-
-@app.route('/chuu/hobbies')
-def chuu_hobbies():
-    return render_template('chuu_hobbies.html', title="Kaitlyn Chau's Hobbies")
-
-@app.route('/chuu/travel')
-def chuu_travel():
-    return render_template('chuu_travel.html', title="Kaitlyn Chau's Travel'")
-
-@app.route('/chuu/experience')
-def chuu_experience():
-    return render_template('chuu_experience.html', title="Kaytlyn's Experience")
-
-@app.route('/chuu/education')
-def chuu_education():
-    return render_template('chuu_education.html', title="Kaitlyn's Education")
-
-@app.route('/diaz')
-def diaz():
-    return render_template('diaz.html', title="Daniel Diaz Portfolio")
 
 
 
